7160a9d63fc8f1fbe5b26304fff3106f85bd1502
atharvams <atharvams009@gmail.com>
2025-06-18 00:19:14 +0530

    Fixed the linting errors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 
 function Header() {
@@ -6,13 +5,12 @@ function Header() {
     <>
       <div className="flex items-center justify-between p-4 rounded-lg">
         <div>
-          <h1 className="text-md font-semibold">Hey! I'm Atharva!</h1>
+          <h1 className="text-md font-semibold">Hey! I&apos;m Atharva!</h1>
           <p className="pt-4 text-zinc-500">
-            You have an Idea! I know how to build it! Let's work together!
+            You have an Idea! I know how to build it! Let&apos;s work together!
           </p>
           <p className="text-sm text-zinc-300">Mail: atharva.vit@gmail.com</p>
         </div>
-        
       </div>
     </>
   );
