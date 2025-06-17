cfb0f9290c795dfbc87ad44abc475eb4bc47efd4
atharvams <atharvams009@gmail.com>
2025-06-18 00:32:43 +0530

    Testing prod branch deployment

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ function Header() {
     <>
       <div className="flex items-center justify-between p-4 rounded-lg">
         <div>
-          <h1 className="text-md font-semibold">Hey! I&apos;m Atharva!</h1>
+          <h1 className="text-md font-semibold">Hey! I&apos;m Atharva! Production</h1>
           <p className="pt-4 text-zinc-500">
             You have an Idea! I know how to build it! Let&apos;s work together!
           </p>
